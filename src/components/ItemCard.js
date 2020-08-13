12525af8f864c5931325ad6a3ec163e1d27497bd
Jason Crowley <crowley.json@gmail.com>
2020-08-13 01:25:08 -0400

    Animate accordion

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import styled from "styled-components"
 
 const Card = styled.li`
-  padding: 1em;
+  padding: 0;
   border: 0.2px solid #eee;
   border-radius: 5px;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.07);
   list-style-type: none;
+  transition: box-shadow 0.1s linear;
 
   &:hover {
     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
@@ -22,20 +23,34 @@ const CardHeader = styled.h2`
 `
 
 const Button = styled.button`
+  padding: 1rem;
   width: 100%;
   height: 100%;
   border: none;
   background-color: transparent;
   text-align: start;
+
+  &:focus {
+    outline: none;
+  }
 `
 
 const CardContent = styled.div`
-  display: ${props => (props.expanded ? "block" : "none")};
-  margin-top: 1.45rem;
+  margin: 0;
+  padding: 0;
+  max-height: ${props => (props.expanded ? props.contentHeight() : 0)}px;
+  overflow: hidden;
+  transition: max-height 0.2s ease-in-out;
+`
+
+const CardContentBox = styled.div`
+  padding: 0 1rem 1rem;
 `
 
 const ItemCard = ({ heading, children }) => {
   const [expanded, setExpanded] = useState(false)
+  const contentRef = useRef(null)
+
   return (
     <Card>
       <CardHeader>
@@ -43,7 +58,13 @@ const ItemCard = ({ heading, children }) => {
           {heading}
         </Button>
       </CardHeader>
-      <CardContent expanded={expanded}>{children}</CardContent>
+      <CardContent
+        expanded={expanded}
+        contentHeight={() => contentRef.current.scrollHeight}
+        ref={contentRef}
+      >
+        <CardContentBox>{children}</CardContentBox>
+      </CardContent>
     </Card>
   )
 }
