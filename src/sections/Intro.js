5b2c256d5d122da7d49f9241b9b50aa5340ce5f3
Jason Crowley <crowley.json@gmail.com>
2020-08-22 18:19:12 -0400

    Add Waves to Intro component

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -1,24 +1,51 @@
 import React from "react"
-
-import Section from "../components/Section"
-import SectionHeading from "../components/SectionHeading"
 import styled from "styled-components"
 
-const IntroSection = styled(Section)`
+import Waves from "../components/Waves"
+
+const WavesContainer = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
-  min-height: 100vh;
-  height: 100vh;
+  margin-bottom: 4rem;
+  height: 65vh;
+  min-height: 65vh;
+  position: relative;
+  background: linear-gradient(
+    60deg,
+    rgba(84, 58, 183, 1) 0%,
+    rgba(0, 172, 193, 1) 100%
+  );
 `
 
-const IntroHeading = styled(SectionHeading)`
-  margin: 0 auto;
+const PositionedWaves = styled(Waves)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+const IntroHeading = styled.h1`
+  font-size: 4rem;
+  color: white;
+
+  padding: 1rem;
+  text-shadow: 0 0 0 rgba(0,0,0,0.6);
+
+  transition: all 0.3s ease;
+
+  &:hover {
+    padding: 2rem;
+    text-shadow: 5px 3px 2px rgba(0,0,0,0.2);
+    transform: translate(-8px, -4px);
+  }
 `
 
 const Intro = () => (
-  <IntroSection>
-    <IntroHeading>Hi, I'm Jason</IntroHeading>
-  </IntroSection>
+  <WavesContainer>
+    <IntroHeading>Hi, I'm Jason Crowley</IntroHeading>
+    <PositionedWaves />
+  </WavesContainer>
 )
 
 export default Intro
