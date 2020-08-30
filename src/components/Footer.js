bfb5f013b9d7c75264a64b6ca1e2f53864a6cb43
Jason Crowley <crowley.json@gmail.com>
2020-08-30 13:34:25 -0400

    Add footer with socials and fontawesome icons

---
+++ src/components/Footer.js
@@ -0,0 +1,67 @@
+import React from "react"
+import styled from "styled-components"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+const Socials = styled.address`
+  margin: 0;
+  padding: 4rem 0;
+  background-color: #1a202c;
+  font-style: normal;
+`
+
+const SocialList = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  padding: 0 1rem;
+  max-width: 960px;
+  list-style-type: none;
+`
+
+const Social = styled.li`
+  margin: 0.1rem 0;
+`
+
+const SocialLink = styled.a.attrs({
+  target: "_blank",
+  rel: "noreferrer",
+})`
+  margin: 0 1rem;
+  color: white;
+  text-decoration: none;
+`
+
+const LinkText = styled.span`
+  margin-left: 0.5rem;
+`
+
+const Footer = () => (
+  <Socials>
+    <SocialList>
+      <Social>
+        <SocialLink href="https://github.com/jason-crowley">
+          <FontAwesomeIcon icon={faGithub} />
+          <LinkText>jason-crowley</LinkText>
+        </SocialLink>
+      </Social>
+      <Social>
+        <SocialLink href="https://www.linkedin.com/in/jason-crowley">
+          <FontAwesomeIcon icon={faLinkedin} />
+          <LinkText>jason-crowley</LinkText>
+        </SocialLink>
+      </Social>
+      <Social>
+        <SocialLink href="mailto:jason_crowley@brown.edu">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <LinkText>jason_crowley@brown.edu</LinkText>
+        </SocialLink>
+      </Social>
+    </SocialList>
+  </Socials>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import Footer from "../components/Footer"
 import SEO from "../components/SEO"
 import Intro from "../sections/Intro"
 import About from "../sections/About"
 import Experience from "../sections/Experience"
 import Projects from "../sections/Projects"
 import Contact from "../sections/Contact"
-
-const Footer = () => (
-  <>
-    &copy; {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
-  </>
-)
 
 const IndexPage = () => (
   <Layout header={<Intro />} footer={<Footer />}>
