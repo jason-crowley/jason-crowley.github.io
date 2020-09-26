083a8c50f15e0bd5a6dc9e117990d3d3d66d4b57
Jason Crowley <crowley.json@gmail.com>
2020-09-26 16:52:16 -0400

    Replace site icon and remove unused images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Jason Crowley Developer Portfolio`,
+    title: `Jason Crowley | Portfolio`,
     description: `A portfolio of projects written in React, JavaScript, Python and more, developed by Jason Crowley`,
     author: `Jason Crowley`,
   },
@@ -21,10 +21,10 @@ module.exports = {
         name: `Jason Crowley Developer Portfolio`,
         short_name: `Jason C. Portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1e90ff`,
+        theme_color: `#1e90ff`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
